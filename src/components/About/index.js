5991ebefe0abf2c4a6bfc7a6b49274df801a4559
noemie-cadet <noemie.cadet@epitech.eu>
2023-09-03 23:39:02 +0400

    correct about me EN and FR

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,7 +2,14 @@ import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
 
 const About = () => {
@@ -35,73 +42,78 @@ const About = () => {
 
   return (
     <>
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={AboutArray}
-            idx={15}
-          />
-        </h1>
-        <p>
-          Je suis une étudiante ambitieuse en informatique de 5ème année à
-          Epitech, ma personnalité se distingue par une passion pour les
-          avancées technologiques. Mon enthousiasme se concentre
-          particulièrement sur le domaine de la technologie 3D, englobant
-          l'impression tridimensionnelle et la création de modèles virtuels. Mon
-          insatiable soif d'apprentissage et d'amélioration me pousse à
-          constamment relever de nouveaux défis, élargissant ainsi en permanence
-          mes compétences et mon savoir-faire.
-        </p>
-        <br />
-        <p>
-          Dotée de compétences en conception de sites Web, j'apporte à mes
-          projets un équilibre harmonieux entre créativités et compétences
-          techniques. Mon bagage de connaissances en Front-end me permet de
-          façonner des expériences numériques qui captivent visuellement et
-          garantissent convivialité et accessibilité. Mon parcours m'a permis de
-          cultiver une approche équilibrée, où chaque élément visuel est
-          soigneusement pensé pour offrir une expérience utilisateur
-          exceptionnelle, tout en étant soutenu par une base technique.
-        </p>
-        <br />
-        <p>
-          Mon intérêt prononcé pour la modélisation en 3D et l'impression
-          tridimensionnelle se traduit par une aptitude à donner vie à des
-          concepts virtuels. Grâce à l'année passée à Keimyung University et à
-          mon expérience personnelle, j'ai appris à naviguer à travers les
-          outils de modélisation, façonnant des créations qui fusionnent
-          esthétisme et fonctionnalité. Mes compétences dans ce domaine me
-          permettent de convertir des idées abstraites en objets virtuels ou en
-          réalités concrètes.
-        </p>
-      </div>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={AboutArray}
+              idx={15}
+            />
+          </h1>
+          <p>
+            Je suis une étudiante ambitieuse en informatique de 5ème année à
+            Epitech, Mon enthousiasme se focalise principalement sur le domaine de la technologie 3D. Mon enthousiasme se concentre
+            particulièrement sur le domaine de la technologie 3D, englobant
+            l'impression 3D et la création de modèles virtuels. Je suis
+            également passionné de front end, le CSS et autres langages de
+            styles sont pour moi des outils pour exprimer ma créativité. Mon
+            insatiable soif d'apprentissage et d'amélioration me pousse à
+            constamment relever de nouveaux défis, élargissant ainsi en
+            permanence mes compétences et mon savoir-faire.
+          </p>
+          <br />
+          <p>
+            Avec des compétences en conception de sites Web, j'apporte à mes
+            projets un mélange harmonieux de créativité et de compétences
+            techniques. Mon bagage en développement Front-end me permet de créer
+            des expériences en ligne visuellement captivantes et conviviales.
+            J'ai développé cette approche équilibrée en travaillant sur divers
+            projets professionnels, académiques et personnels, où chaque élément
+            visuel est méticuleusement pensé pour offrir une expérience
+            utilisateur exceptionnelle.
+          </p>
+          <br />
+          <p>
+            Ma passion pour la modélisation en 3D et l'impression 3D découle de
+            ma fascination pour la transformation d'idées virtuelles en objets
+            tangibles. L'année que j'ai passée à Keimyung University ainsi que
+            mes expériences personnelles m'ont permis d'explorer en profondeur
+            les outils de modélisation. Cette immersion m'a aidé à développer
+            une expertise solide dans la création de modèles tridimensionnels.
+            Ce qui rend cette expérience encore plus excitante, c'est ma
+            capacité à concrétiser des concepts abstraits. Mes compétences en
+            modélisation 3D me permettent de prendre une idée et de la donner vie sous la forme d'un modèle
+            virtuel détaillé. De plus, grâce à l'impression 3D, je peux
+            transcender le numérique pour créer des objets physiques concrets à
+            partir de ces modèles virtuels.
+          </p>
+        </div>
 
-      <div className="stage-cube-cont">
-        <div className="cubespinner">
-          <div className="face1">
-            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
-          </div>
-          <div className="face2">
-            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-          </div>
-          <div className="face3">
-            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-          </div>
-          <div className="face4">
-            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-          </div>
-          <div className="face5">
-            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-          </div>
-          <div className="face6">
-            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <Loader type="ball-scale" />
+      <Loader type="ball-scale" />
     </>
   )
 }
--- src/components/EN/About/index.js
+++ src/components/EN/About/index.js
@@ -2,21 +2,19 @@ import './index.scss'
 import AnimatedLetters from '../../AnimatedLetters'
 import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
-  const AboutArray = [
-    'A',
-    'b',
-    'o',
-    'u',
-    't',
-    ' ',
-    'm',
-    'e'
-  ]
+  const AboutArray = ['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']
 
   useEffect(() => {
     setTimeout(() => {
@@ -26,73 +24,76 @@ const About = () => {
 
   return (
     <>
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={AboutArray}
-            idx={15}
-          />
-        </h1>
-        <p>
-        I am an ambitious student in computer science from 5th year to
-          Epitech, my personality is distinguished by a passion for
-          technological advances. My enthusiasm is focused
-          particularly in the field of 3D technology, encompassing
-          three-dimensional printing and the creation of virtual models. My
-          insatiable thirst for learning and improvement pushes me to
-          constantly meet new challenges, thus continuously expanding
-          my skills and my know-how.
-        </p>
-        <br />
-        <p>
-        With web design skills, I bring to my
-          projects a harmonious balance between creativity and skills
-          techniques. My background in Front-end allows me to
-          Shape digital experiences that visually and
-          guarantee user-friendliness and accessibility. My background allowed me to
-          cultivate a balanced approach, where every visual element is
-          carefully designed to offer a user experience
-          exceptional, while being supported by a technical base.
-        </p>
-        <br />
-        <p>
-        My strong interest in 3D modeling and printing
-          the ability to give life to
-          virtual concepts. Thanks to last year at Keimyung University and
-          my personal experience, I learned to navigate through the
-          modeling tools, shaping creations that merge
-          aesthetics and functionality. My skills in this area
-          convert abstract ideas into virtual objects or
-          concrete realities.
-        </p>
-      </div>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={AboutArray}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am an ambitious student in computer science from 5th year to
+            Epitech, My enthusiasm focuses mainly on the field of 3D technology.
+            My enthusiasm focuses on particularly in the field of 3D technology,
+            encompassing 3D printing and creating virtual models. I am also
+            passionate about front end, CSS and other languages of styles are
+            for me tools to express my creativity. My insatiable thirst for
+            learning and improvement pushes me to constantly meet new
+            challenges, thus expanding by my skills and knowledge.
+          </p>
+          <br />
+          <p>
+            With web design skills, I bring to my projects a harmonious mix of
+            creativity and skills techniques. My background in Front-end
+            development allows me to create Visually engaging and user-friendly
+            online experiences. I developed this balanced approach by working on
+            various professional, academic and personal projects, where every
+            element visual is meticulously designed to offer an experience
+            exceptional user.
+          </p>
+          <br />
+          <p>
+          My passion for 3D modeling and 3D printing stems from
+            my fascination for transforming virtual ideas into objects
+            The year I spent at Keimyung University as well as
+            my personal experiences allowed me to explore in depth
+            modelling tools. This immersion helped me to develop
+            solid expertise in the creation of three-dimensional models.
+            What makes this experience even more exciting is my
+            ability to bring abstract concepts to life. My
+            3D modeling allow me to take an idea and bring it to life in the form of a model
+            virtual detailed. Moreover, thanks to 3D printing, I can
+            transcend the digital to create concrete physical objects to
+            from these virtual models.
+          </p>
+        </div>
 
-      <div className="stage-cube-cont">
-        <div className="cubespinner">
-          <div className="face1">
-            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
-          </div>
-          <div className="face2">
-            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-          </div>
-          <div className="face3">
-            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-          </div>
-          <div className="face4">
-            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-          </div>
-          <div className="face5">
-            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-          </div>
-          <div className="face6">
-            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <Loader type="ball-scale" />
+      <Loader type="ball-scale" />
     </>
   )
 }
